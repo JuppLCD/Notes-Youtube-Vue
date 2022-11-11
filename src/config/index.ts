07d93c936a977eb3975ca8e7b60b291e24874380
JuppLCD <lorenzocanovas207@gmail.com>
2022-11-10 21:18:49 -0300

    Environment Variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,6 @@
 const TOKEN_KEY_LOCAL_STORAGE = 'accessToken';
 
-export { TOKEN_KEY_LOCAL_STORAGE };
+const URL_BACKEND: string = import.meta.env.VITE_HOST_BAKEND;
+const URL_API_BACKEND = URL_BACKEND + '/api';
+
+export { TOKEN_KEY_LOCAL_STORAGE, URL_BACKEND, URL_API_BACKEND };
