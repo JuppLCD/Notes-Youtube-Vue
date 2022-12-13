6c130d5f4138d0042bf5ba3a4f6f63f63698d767
JuppLCD <lorenzocanovas207@gmail.com>
2022-12-12 21:16:40 -0300

    Using component ModalEditNote and hook useModalEditNote in NoteListDetailsPage and VideoNotePage

--- src/views/NoteListDetailsPage.vue
+++ src/views/NoteListDetailsPage.vue
@@ -4,9 +4,12 @@ import { computed } from 'vue';
 import { useStoreVuex } from '@/store';
 import { useRoute, useRouter } from 'vue-router';
 
+import useModalEditNote from '@/hooks/useModalEditNote';
+
 import Container from '@/components/Container.vue';
 import CardNote from '@/components/CardNote.vue';
 import Button from '@/components/Button.vue';
+import ModalEditNote from '@/components/ModalEditNote.vue';
 
 const route = useRoute();
 const router = useRouter();
@@ -36,6 +39,8 @@ const deleteNoteList = () => {
 		router.push({ name: 'myListsOfNotes' });
 	}
 };
+
+const { open, handleSubmit, editInputs, showModalEditNote, closeModal } = useModalEditNote();
 </script>
 
 <template>
@@ -46,12 +51,19 @@ const deleteNoteList = () => {
 				<p><span class="text-lg font-bold">Description: </span> {{ noteList.description }}</p>
 				<Button @click="deleteNoteList" color="red" type="button" class="m-2 ml-auto">Delete List</Button>
 				<ul>
-					<CardNote v-for="note in noteList.notes" :key="note.id" :note="note" @deleteNote="deleteNote" />
+					<CardNote
+						v-for="note in noteList.notes"
+						:key="note.id"
+						:note="note"
+						@deleteNote="deleteNote"
+						@showModalEditNote="showModalEditNote"
+					/>
 				</ul>
 			</template>
 			<template v-else>
 				<h1 class="text-3xl mb-2">{{ route.params.noteListId }}</h1>
 			</template>
 		</Container>
+		<ModalEditNote :open="open" :editInputs="editInputs" @handleSubmit="handleSubmit" @closeModal="closeModal" />
 	</main>
 </template>
--- src/views/VideoNotes/Notes.vue
+++ src/views/VideoNotes/Notes.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
-import { useStoreVuex } from '@/store';
 import { computed } from '@vue/reactivity';
 
+import { useStoreVuex } from '@/store';
+
+import useModalEditNote from '@/hooks/useModalEditNote';
+
 import CardNote from '@/components/CardNote.vue';
+import ModalEditNote from '@/components/ModalEditNote.vue';
 
 const store = useStoreVuex();
 const notes = computed(() => store.state.videoToAnalyze.notes);
@@ -10,13 +14,22 @@ const notes = computed(() => store.state.videoToAnalyze.notes);
 const deleteNote = (id: number): void => {
 	store.dispatch('videoToAnalyze/deleteNote', { id });
 };
+
+const { open, handleSubmit, editInputs, showModalEditNote, closeModal } = useModalEditNote();
 </script>
 
 <template>
 	<section class="py-2">
 		<h2 class="text-center text-2xl my-2">Notes of the YT Video</h2>
 		<ul v-if="notes">
-			<CardNote v-for="note in notes" :key="note.id" :note="note" @deleteNote="deleteNote" />
+			<CardNote
+				v-for="note in notes"
+				:key="note.id"
+				:note="note"
+				@deleteNote="deleteNote"
+				@showModalEditNote="showModalEditNote"
+			/>
+			<ModalEditNote :open="open" :editInputs="editInputs" @handleSubmit="handleSubmit" @closeModal="closeModal" />
 		</ul>
 		<template v-else>
 			<p class="text-center">There are no notes, place a video and create your note...</p>
