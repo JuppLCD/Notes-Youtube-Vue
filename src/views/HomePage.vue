fdb65dc583fdd22d0c30288b6d8735d704e3d127
JuppLCD <lorenzocanovas207@gmail.com>
2023-01-18 15:26:24 -0300

    Modifying the text of the hero section in views/HomePage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -11,18 +11,20 @@ import { APP_NAME } from '@/config';
 					<div
 						class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-8 md:mb-0 items-center text-center"
 					>
-						<h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium">
+						<h1 class="title-font xl:text-5xl sm:text-4xl text-3xl mb-4 font-medium">
 							Enjoy a good video <br class="hidden lg:inline-block" />and save your best ideas
 						</h1>
 						<p class="mb-8 leading-relaxed">
 							{{
-								`${APP_NAME} is an app that allows you to watch a YouTube video without distractions, with the aim that you can take all the ideas and place them in notes, organizing them in lists of notes`
+								`${APP_NAME} is an app that allows you to watch a YouTube video without distractions, with the aim that you can take all the ideas and place them in notes, organizing them in lists of notes.`
 							}}
+							<br />
+							<span class="font-bold"
+								>Avoid the clutter of paper notes and organize your thoughts about what you see or hear.</span
+							>
 						</p>
-						<p class="mb-8 leading-relaxed">Avoid the disasters of paper notes</p>
 					</div>
 					<div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-						<!-- 568 × 619 px -->
 						<img
 							class="object-cover object-center rounded"
 							alt="hero"
