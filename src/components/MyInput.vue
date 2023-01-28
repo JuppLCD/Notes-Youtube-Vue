bd595381b3f2c72cffffd636c066c00ca783437a
JuppLCD <lorenzocanovas207@gmail.com>
2023-01-27 21:28:42 -0300

    Fix bug: Modifying MyInput component

--- src/components/MyInput.vue
+++ src/components/MyInput.vue
@@ -6,6 +6,12 @@ interface FormInput extends InputHTMLAttributes {
 }
 
 const { modelValue } = defineProps<FormInput>();
+
+const emit = defineEmits(['update:modelValue']);
+
+const handleInput = (event: Event) => {
+	emit('update:modelValue', (event.target as HTMLInputElement).value);
+};
 </script>
 
 <template>
@@ -14,7 +20,7 @@ const { modelValue } = defineProps<FormInput>();
 		<input
 			v-bind="$attrs"
 			:value="modelValue"
-			@input="$emit('update:modelValue', $event.target.value)"
+			@input="handleInput"
 			class="block w-full p-2.5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 		/>
 	</label>
