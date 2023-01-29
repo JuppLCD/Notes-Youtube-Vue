8e756bad36d70e4504fb25304a6ba4f603e70991
JuppLCD <lorenzocanovas207@gmail.com>
2023-01-29 18:42:53 -0300

    Feature "watch video" in component CardNote

--- src/components/CardNote.vue
+++ src/components/CardNote.vue
@@ -1,13 +1,26 @@
 <script setup lang="ts">
-import { Note } from '@/types/Note';
+import { useStoreVuex } from '@/store';
 
 import Button from '@/components/Button.vue';
+
+import { Note } from '@/types/Note';
+import { useRouter } from 'vue-router';
 
 const { note } = defineProps<{ note: Note }>();
 const emits = defineEmits<{
 	(e: 'deleteNote', id: number): void;
 	(e: 'showModalEditNote', note: Note): void;
 }>();
+
+const store = useStoreVuex();
+const router = useRouter();
+
+const renderButtonWatchVideo = router.currentRoute.value.name !== 'YTVideoNotes';
+const handleWatchVideo = () => {
+	const idVideo = note.idYTVideo;
+	store.dispatch('videoToAnalyze/setIdYoutubeVideo', { idVideo });
+	router.push({ name: 'YTVideoNotes' });
+};
 </script>
 
 <template>
@@ -15,6 +28,9 @@ const emits = defineEmits<{
 		<h3 class="text-xl my-2">{{ note.title }}</h3>
 		<p class="mb-3">{{ note.text }}</p>
 		<div class="flex justify-end">
+			<Button class="mr-2" type="button" color="white" @click="handleWatchVideo" v-if="renderButtonWatchVideo">
+				Watch video
+			</Button>
 			<Button class="mr-2" type="button" @click="emits('showModalEditNote', note)"> Edit </Button>
 			<Button @click="emits('deleteNote', note.id)" color="red" type="button"> Delete </Button>
 		</div>
