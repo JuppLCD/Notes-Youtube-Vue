a701fd7baeddaad653bd126265b9ae714c20db0b
JuppLCD <lorenzocanovas207@gmail.com>
2023-02-04 19:05:50 -0300

    Changing styles

--- src/views/SessionPage.vue
+++ src/views/SessionPage.vue
@@ -40,22 +40,25 @@ const handleSubmit = () => {
 <template>
 	<main>
 		<Container>
-			<form @submit.prevent="handleSubmit">
+			<form @submit.prevent="handleSubmit" class="sm:px-10 md:px-20 lg:px-30">
 				<MyInput type="email" placeholder="example@example.com" v-model="sessionInputs.email"> Email </MyInput>
 
-				<MyInput type="password" placeholder="******" v-model="sessionInputs.password"> Password </MyInput>
+				<MyInput type="password" placeholder="******************" v-model="sessionInputs.password"> Password </MyInput>
 
 				<template v-if="sessionType === 'signup'">
-					<MyInput type="password" placeholder="******" v-model="sessionInputs.confirmPassword">
+					<MyInput type="password" placeholder="******************" v-model="sessionInputs.confirmPassword">
 						Confirm password
 					</MyInput>
 					<MyInput type="text" placeholder="userExample" v-model="sessionInputs.name"> UserName </MyInput>
+					<div class="text-blue-400 hover:text-purple-500">
+						<RouterLink :to="{ name: 'login' }"> Do you already have an account? Log in here </RouterLink>
+					</div>
 				</template>
-				<div v-else class="my-2 text-blue-400 hover:text-purple-500">
+				<div v-else class="text-blue-400 hover:text-purple-500">
 					<RouterLink :to="{ name: 'signup' }"> You do not have an account? Sign up </RouterLink>
 				</div>
 
-				<Button type="submit">
+				<Button type="submit" class="mt-2">
 					{{ sessionType === 'login' ? 'Log in' : 'Sign up' }}
 				</Button>
 			</form>
